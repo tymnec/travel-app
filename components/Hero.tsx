982b6070f22bb5f79c6907ec3d2afaa610d6b33c
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 13:06:56 +0530

    add 5 stars

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 
@@ -22,6 +24,15 @@ const Hero = () => {
           seeing the incorruptible beauty of nature. We can help you on an
           adventure around the world in just one app.
         </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image src={"/star.svg"} alt="star" width={24} height={24} />
+              ))}
+          </div>
+        </div>
       </div>
     </section>
   );
