a567fbabf101f2f1bd1a7b03352d055af082b952
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 12:42:26 +0530

    add constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { NAV_LINKS } from "@/constants/index";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,15 @@ const Navbar = () => {
       {/* Navbar */}
       <Link href={"/"}>
         <Image src={"hilink-logo.svg"} alt="logo" width={100} height={100} />
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => {
+            return (
+              <Link href={link.href} key={link.key}>
+                {link.label}
+              </Link>
+            );
+          })}
+        </ul>
       </Link>
     </nav>
   );
