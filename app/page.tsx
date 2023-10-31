f6c7f68850c5b6935ade31619c816bf6bb5adc8f
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 12:56:32 +0530

    import sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+
 export default function App(){
   return (
     <>
-      hello
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
     </>
   )
 }
