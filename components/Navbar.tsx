2c708563eb3e2937e3ae49d330bc8b759cd076f3
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 12:38:00 +0530

    logo complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       {/* Navbar */}
       <Link href={"/"}>
         <Image src={"hilink-logo.svg"} alt="logo" width={100} height={100} />
