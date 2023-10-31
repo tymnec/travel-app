a5eda3ac55e426d878d38b28bd7a6fea1e39b2bd
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 12:59:54 +0530

    left side and map in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
 
-export default Hero
+      {/* Left Side */}
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image src={"/camp.svg"} alt="camp" width={50} height={50} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
