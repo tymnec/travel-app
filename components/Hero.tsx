71c65e6fd940baaf58e0a7c19b8e3f6e62867a18
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 13:13:13 +0530

    add how we work ? button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,8 @@ const Hero = () => {
         </div>
 
         <div className="flex w-full gap-3 sm:flex-row">
-          <Button type="button" title="Download" variant="btn_green" />
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button type="button" title="How we work?" icon="/play.svg" variant="btn_white_text" />
         </div>
       </div>
     </section>
