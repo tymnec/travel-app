34860030b1b00a3c99b85e67da2bc71c34d6a169
Nikhil Sarwara <tymnec@gmail.com>
2023-10-31 12:36:56 +0530

    add images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
       {/* Navbar */}
-      <Link href={"/"}></Link>
+      <Link href={"/"}>
+        <Image src={"hilink-logo.svg"} alt="logo" width={100} height={100} />
+      </Link>
     </nav>
   );
 };
