d37f73fb154b70bc602bbf5938023517ab10920d
Nikhil Sarwara <tymnec@gmail.com>
2023-11-01 14:53:47 +0530

    add camp photos

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,26 +1,43 @@
+import Image from "next/image";
+
 interface CampProps {
-    backgroundImage: string
-    title: string
-    subtitle: string
-    peopleJoined: string
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
 }
 
-const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
-    return ( 
-        <div>
-            
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src={"/folded-map.svg"} alt="camp" width={50} height={50} />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const Camp = () => {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollar flex h-[340px] w-full items-start justify-start overflow-x-scroll gap-8 lg:h-[400px] xl:h-[640px]">
-        <CampSite backgroundImage="bg-bg-img-1"
-        title="Putuk Truno Camp"
-        subtitle="Putuk Truno Camp"
-        peopleJoined="50+ joined"
+      <div className="hide-scrollar flex h-[340px] w-full items-start justify-start overflow-x-hidden gap-8 lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Putuk Truno Camp"
+          peopleJoined="50+ joined"
+        />
       </div>
     </section>
   );
