402f50e2cce25bedc199c7999f1d417c7a9892b6
Nikhil Sarwara <tymnec@gmail.com>
2023-11-01 15:04:16 +0530

    add more camp sites

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,4 @@
+import { PEOPLE_URL } from "@/constants";
 import Image from "next/image";
 
 interface CampProps {
@@ -22,6 +23,25 @@ const CampSite = ({
           <div className="rounded-full bg-green-50 p-4">
             <Image src={"/folded-map.svg"} alt="camp" width={50} height={50} />
           </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+            <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+          </span>
         </div>
       </div>
     </div>
@@ -30,10 +50,16 @@ const CampSite = ({
 
 const Camp = () => {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollar flex h-[340px] w-full items-start justify-start overflow-x-hidden gap-8 lg:h-[400px] xl:h-[640px]">
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 overflow-hidden">
+      <div className="flex hide-scrollbar h-[340px] w-full items-start justify-start overflow-x-scroll gap-8 lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Putuk Truno Camp"
+          peopleJoined="50+ joined"
+        />
+        <CampSite
+          backgroundImage="bg-bg-img-2"
           title="Putuk Truno Camp"
           subtitle="Putuk Truno Camp"
           peopleJoined="50+ joined"
